121a9bf470433b255d27d36b87da4f1bbecdad1b
haisum <haisumbhatti@gmail.com>
2016-08-01 23:15:43 +0500

    Making expiry date 15 August

--- smpp/license/expire.go
+++ smpp/license/expire.go
@@ -10,13 +10,20 @@ import (
 // CheckExpiry expires after 1st july
 // This function must be called in a thread otherwise it will block eternally.
 func CheckExpiry() {
+	msgPrinted := false
 	for {
 		now := time.Now().UTC()
-		if now.Unix() > time.Date(2016, time.July, 1, 0, 0, 0, 0, now.Location()).Unix() {
-			log.Error("License has expired. Please purchase full version to continue usage.")
+		if now.Unix() > time.Date(2016, time.August, 15, 0, 0, 0, 0, now.Location()).Unix() {
+			if !msgPrinted {
+				log.Error("License has expired. Please purchase full version to continue usage.")
+				msgPrinted = true
+			}
 			os.Exit(2)
 		} else {
-			log.Info("This license is valid till 1st July.")
+			if !msgPrinted {
+				log.Info("This license is valid till 1st July.")
+				msgPrinted = true
+			}
 		}
 		time.Sleep(time.Minute * 5)
 	}
